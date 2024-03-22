b169b128c1af88ae577536928b9d0b79fb8f4455
hanxuelei <hanxuelei@lejingling.cn>
2024-03-23 02:35:14 +0800

    使用 consul 当做配置中心

--- cloud-provider-payment/src/main/java/com/learngenie/cloud/Main8001.java
+++ cloud-provider-payment/src/main/java/com/learngenie/cloud/Main8001.java
@@ -1,8 +1,11 @@
 package com.learngenie.cloud;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
 
 // 入住注册中心
@@ -12,5 +15,19 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class Main8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
+    }
+
+    @Value("${info}")
+    private String info;
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+
+            @Override
+            public void run(String... args) throws Exception {
+                System.out.println("get info from consul" + info);
+            }
+        };
     }
 }
