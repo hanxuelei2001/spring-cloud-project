c38ca85d5c7a75991aa00566d22a30755159e724
hanxuelei <hanxuelei@lejingling.cn>
2024-03-23 20:38:19 +0800

    loadBalance 原理为 DiscoveryClient

--- cloud-consumer-order80/src/main/java/com/learngenie/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/learngenie/cloud/controller/OrderController.java
@@ -1,14 +1,22 @@
 package com.learngenie.cloud.controller;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.learngenie.cloud.entities.PayDto;
 import com.learngenie.cloud.response.ResultData;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 
 @RestController
@@ -17,6 +25,7 @@ public class OrderController {
 
     //public static final String PaymentSrv_URL = "http://localhost:8001/api/v1/pay";
     public static final String PaymentSrv_URL = "http://cloud-payment-service/api/v1/pay";
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -53,4 +62,24 @@ public class OrderController {
         return restTemplate.getForObject(PaymentSrv_URL + "/get/all", ResultData.class);
     }
 
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/consumer/discovery")
+    public String discovery() {
+        // 获取所有的 service
+        List<String> services = discoveryClient.getServices();
+        services.forEach(log::info);
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        if (CollectionUtil.isNotEmpty(instances)) {
+            instances.forEach(serviceInstance -> {
+                log.info("serviceId: {}, host: {}, port: {}, uri: {}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort(), serviceInstance.getUri());
+            });
+
+            return instances.get(0).getServiceId()+ ":" + instances.get(0).getHost();
+        }
+        return "";
+    }
 }
