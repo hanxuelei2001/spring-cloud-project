91caedb59db91e3a83f519caa9a4131b25d7b167
hanxuelei <hanxuelei@lejingling.cn>
2024-03-23 20:48:13 +0800

    loadBalance 算法修改为 random

--- cloud-consumer-order80/src/main/java/com/learngenie/cloud/config/RestTemplateConfig.java
+++ cloud-consumer-order80/src/main/java/com/learngenie/cloud/config/RestTemplateConfig.java
@@ -1,10 +1,19 @@
 package com.learngenie.cloud.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
+// 修改 loadBalance 的算法
+@LoadBalancerClient(value = "cloud-payment-service", configuration = RestTemplateConfig.class)
 @Configuration
 public class RestTemplateConfig {
 
@@ -12,5 +21,14 @@ public class RestTemplateConfig {
     @LoadBalanced // 由于 consul 默认支持 loadBalance 所以这里要在 restTemplate 这里添加上
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment,
+                                                            LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(loadBalancerClientFactory
+                .getLazyProvider(name, ServiceInstanceListSupplier.class),
+                name);
     }
 }
