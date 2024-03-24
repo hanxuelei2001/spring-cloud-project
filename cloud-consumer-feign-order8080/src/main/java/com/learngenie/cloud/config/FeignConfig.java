ceb7aabbf54eefef5e66d94684546c6517a216cf
hanxuelei <hanxuelei@lejingling.cn>
2024-03-24 22:12:18 +0800

    注释阻塞代码

--- cloud-consumer-feign-order8080/src/main/java/com/learngenie/cloud/config/FeignConfig.java
+++ cloud-consumer-feign-order8080/src/main/java/com/learngenie/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.learngenie.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +13,11 @@ public class FeignConfig {
          return Retryer.NEVER_RETRY; // 默认是不进行 retry 的
         // 最大请求次数为 3 ( 1 + 2), 初始间隔为 100 ms，重试间隔最大为 1 s
         //return new Retryer.Default(100, 1, 3);
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        // 开启内容最详细的日志
+        return Logger.Level.FULL;
     }
 }
