4f52705c738c55053405e9b07187524e90f29cf0
hanxuelei <hanxuelei@lejingling.cn>
2024-03-26 21:06:58 +0800

    resilience4j 限流配置

--- cloud-consumer-feign-order8080/src/main/java/com/learngenie/cloud/controller/OrderCircuitController.java
+++ cloud-consumer-feign-order8080/src/main/java/com/learngenie/cloud/controller/OrderCircuitController.java
@@ -3,6 +3,7 @@ package com.learngenie.cloud.controller;
 import com.learngenie.cloud.apis.PayFeignApi;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.annotation.Resource;
 import org.apache.hc.core5.concurrent.CompletedFuture;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,18 +40,25 @@ public class OrderCircuitController {
     //    return payFeignApi.myBulkhead(id);
     //}
 
-    @GetMapping(value = "/feign/pay/bulkhead/{id}")
-    @Bulkhead(name = "cloud-payment-service", fallbackMethod = "myCircuitThreadFallback", type = Bulkhead.Type.THREADPOOL)
-    public CompletableFuture<String> myBulkhead2(@PathVariable("id") Integer id) throws InterruptedException {
-        TimeUnit.SECONDS.sleep(10);
-        return CompletableFuture.supplyAsync(() -> {
-            return payFeignApi.myBulkhead(id);
-        });
-    }
+    //@GetMapping(value = "/feign/pay/bulkhead/{id}")
+    //@Bulkhead(name = "cloud-payment-service", fallbackMethod = "myCircuitThreadFallback", type = Bulkhead.Type.THREADPOOL)
+    //public CompletableFuture<String> myBulkhead2(@PathVariable("id") Integer id) throws InterruptedException {
+    //    TimeUnit.SECONDS.sleep(10);
+    //    return CompletableFuture.supplyAsync(() -> {
+    //        return payFeignApi.myBulkhead(id);
+    //    });
+    //}
+    //
+    //public CompletableFuture<String> myCircuitThreadFallback(Integer id, Throwable throwable) {
+    //    return CompletableFuture.supplyAsync(() -> {
+    //        return id + "myCircuitFallback 系统繁忙，请稍后重试";
+    //    });
+    //}
 
-    public CompletableFuture<String> myCircuitThreadFallback(Integer id, Throwable throwable) {
-        return CompletableFuture.supplyAsync(() -> {
-            return id + "myCircuitFallback 系统繁忙，请稍后重试";
-        });
+
+    @GetMapping(value = "/feign/pay/ratelimit/{id}")
+    @RateLimiter(name = "cloud-payment-service", fallbackMethod = "myCircuitFallback")
+    public String myBulkhead2(@PathVariable("id") Integer id) throws InterruptedException {
+        return payFeignApi.myBulkhead(id);
     }
 }
