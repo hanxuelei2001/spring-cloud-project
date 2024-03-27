1a887f90de24fd801a66b8648a037b9a39214427
hanxuelei <hanxuelei@lejingling.cn>
2024-03-27 22:40:45 +0800

    gateway 配置 predicted

---
+++ cloud-gateway-9527/src/main/java/com/learngenie/cloud/AfterZoneTime.java
@@ -0,0 +1,11 @@
+package com.learngenie.cloud;
+
+import java.time.ZonedDateTime;
+
+public class AfterZoneTime {
+    // After 的时候填写的时间为这个
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
